0ccb14c814ce5b75b4b3e3f4580cfc4e6d1586b4
lanou3g <lanou3g@lanou3gdeMac-mini.local>
2015-11-09 09:53:30 +0800

    的可靠的肯定

---
+++ IT小春雷/IT小春雷/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IT小春雷
+//
+//  Created by lanou3g on 15/11/9.
+//  Copyright © 2015年 Dr.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
